edf8923cb1a61ecdebb1e3c6f76d72df81cb700e
janae.wel <janae.wel@gmail.com>
2022-08-19 14:54:48 -0400

    started routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,7 +5,9 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
+  Product.findAll({
+    attributes: ['id']
+  })
   // be sure to include its associated Category and Tag data
 });
 
